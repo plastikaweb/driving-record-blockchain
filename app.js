e885b0c4d890359b49c9fa9e9c6fba21bbda83f5
Carlos Matheu Armengol <carlos@stage.tech>
2018-09-30 12:39:55 +0200

    add mine and blockchain route endpoints

--- app.js
+++ app.js
@@ -6,23 +6,20 @@ const Block = require('./block');
 const Blockchain = require('./blockchain');
 const Transaction = require('./transaction');
 let transactions = [];
+const blockChain = new Blockchain(new Block());
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('hello world'));
 
 app.get('/blockchain', (req, res) => {
-  const genesisBlock = new Block();
-  const blockChain = new Blockchain(genesisBlock);
-  const transaction = new Transaction('Mary', 'Jerry', 100);
-  const block = blockChain.setNextBlock([transaction]);
-  blockChain.addBlock(block);
-  const transaction2 = new Transaction('Charles', 'Mary', 55);
-  const transaction3 = new Transaction('John', 'Eva', 15);
-  const block2 = blockChain.setNextBlock([transaction2, transaction3]);
-  blockChain.addBlock(block2);
-
   res.json(blockChain);
+});
+
+app.get('/mine', (req, res) => {
+  const block = blockChain.setNextBlock(transactions);
+  blockChain.addBlock(block);
+  res.json(block);
 });
 
 app.post('/transactions', (req, res) => {
