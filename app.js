3fc4b535412b91c81819da91302436f523c5e036
Carlos Matheu Armengol <carlos@stage.tech>
2018-09-30 12:24:09 +0200

    transaction route to add transactions

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const Block = require('./block');
 const Blockchain = require('./blockchain');
 const Transaction = require('./transaction');
+let transactions = [];
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('hello world'));
+
 app.get('/blockchain', (req, res) => {
   const genesisBlock = new Block();
   const blockChain = new Blockchain(genesisBlock);
@@ -19,4 +24,12 @@ app.get('/blockchain', (req, res) => {
 
   res.json(blockChain);
 });
+
+app.post('/transactions', (req, res) => {
+  const { to, from, amount } = req.body;
+  transactions = [...transactions, new Transaction(from, to, amount)];
+
+  res.json(transactions);
+});
+
 app.listen(3000, () => console.log('server running'));
